d862be740d85117ddd07e4b44fd3c4e241d27379
atilayeseN <atilayesen@hotmail.com>
2022-07-09 00:22:56 +0300

    remove redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import "./App.scss";
-import { useSelector, useDispatch } from "react-redux";
-import { setMembers } from "./store/members";
 import MemberList from "./Components/MemberList/MemberList";
 import Button from "./Components/Button/Button";
 import { useState } from "react";
@@ -10,38 +8,37 @@ import { getMembers } from "./Services/get";
 import { useQuery } from "react-query";
 
 function App() {
-  const { members } = useSelector((state) => state.members);
   const [inputModalState, setinputModalState] = useState(false);
 
   const { isLoading, isError, error, data } = useQuery("members", getMembers);
-
-  const dispatch = useDispatch();
 
   const manageModal = () => {
     setinputModalState((modalState) => !modalState);
   };
 
   
-  if (isLoading) return ("LOADING");
+  if (isLoading) return (<div>
+    <h1>Loading</h1>
+  </div>);
   if (isError) return <pre>{error}</pre>;
   if (data) {
-    dispatch(setMembers(data));
+    return (
+      <div className="App">
+        <h1>EMPLOYEE OF MONTH</h1>
+        <MemberList members={data}></MemberList>
+  
+        <Button event={manageModal}>Add Member</Button>
+        {inputModalState && (
+          <Modal manageModal={manageModal}>
+            <Input manageModal={manageModal} members={data}></Input>
+          </Modal>
+        )}
+      </div>
+    );
   }
 
 
-  return (
-    <div className="App">
-      <h1>EMPLOYEE OF MONTH</h1>
-      <MemberList members={members}></MemberList>
-
-      <Button event={manageModal}>Add Member</Button>
-      {inputModalState && (
-        <Modal manageModal={manageModal}>
-          <Input manageModal={manageModal} members={members}></Input>
-        </Modal>
-      )}
-    </div>
-  );
+  
 }
 
 export default App;
